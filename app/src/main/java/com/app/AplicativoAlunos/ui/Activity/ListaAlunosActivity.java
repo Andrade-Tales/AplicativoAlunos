03a263b727bc10f0bfed9cc02ba026715700a6f6
Andrade-Tales <selatedardna.1476@gmail.com>
2023-08-24 22:57:19 -0300

    Alterado titulo, adicionado botao e nova atividade

--- app/src/main/java/com/app/AplicativoAlunos/ui/Activity/ListaAlunosActivity.java
+++ app/src/main/java/com/app/AplicativoAlunos/ui/Activity/ListaAlunosActivity.java
@@ -1,11 +1,10 @@
-package com.app.AplicativoAlunos;
+package com.app.AplicativoAlunos.ui.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.app.listadealunos.R;
 
@@ -13,23 +12,24 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
+
+        // Alterando titulo do app
+        setTitle("Lista de alunos");
 
         // Criando lista de alunos
         List<String> alunos = new ArrayList<>
                 (Arrays.asList("Alex", "Fran", "José", "Maria", "Ana"));
 
         // Componente ListView (capacidade de compor outras views)
-        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
 
         // Usando o adapter (vínculo dos dados com base em uma view), (adapter views)
         listaDeAlunos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos));
-
-
     }
 }
