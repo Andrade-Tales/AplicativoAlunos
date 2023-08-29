49f0ed7043bec1cef6792c1e65a9cbc1bfbb061b
Andrade-Tales <selatedardna.1476@gmail.com>
2023-08-29 15:01:45 -0300

    Adicionado Listener

--- app/src/main/java/com/app/aplicativoalunos/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/app/aplicativoalunos/ui/activity/ListaAlunosActivity.java
@@ -1,14 +1,20 @@
 package com.app.aplicativoalunos.ui.activity;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.app.aplicativoalunos.dao.AlunoDao;
+import com.app.aplicativoalunos.model.Aluno;
 import com.app.listadealunos.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -25,11 +31,13 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
-
         // Alterando titulo do app bar
         setTitle(TITULO_APPBAR);
-
+        // Botão adicionar aluno
         configuraFabNovoAluno();
+        // Setando dois alunos para (visualização)
+        dao.salva(new Aluno("Alex", "999999999", "alex@gmail.com"));
+        dao.salva(new Aluno("Fran", "999999999", "fran@gmail.com"));
 
     }
 
@@ -63,6 +71,15 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
         // Usando o adapter (vínculo dos dados com base em uma view), (adapter views)
         listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dao.todos()));
+
+        // Adicionando Listener
+        // Exibição de mensagem com Log
+        listaDeAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int posicao, long id) {
+                Log.i("Posição do aluno", "" + posicao);
+            }
+        });
     }
 
 }
